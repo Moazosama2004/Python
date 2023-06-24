75730782267cc9bcb1fe94d9e9d0c9f1b1bafc1b
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-06-24 20:51:47 +0300

    Assignment 3 Done

---
+++ Assignment_3.py
@@ -0,0 +1,37 @@
+# Assignment 1
+
+print(type(10))
+print(type(10.5))
+print(type(10+5j))
+print("=" * 30)
+
+# Assignment 2
+
+cx = 1+2j
+
+print(f"Imaginary Part Is : {cx.imag}")
+print(f"Real Part Is : {cx.real}")
+print("=" * 30)
+
+# Assignment 3
+
+num = 10
+
+print("{:.10f}".format(num))
+print("%0.10f" % (num))
+print("=" * 30)
+
+# Assignment 4
+
+num = 159.650
+
+print(int(num))
+print(type(int(num)))
+
+# Assignment 5
+
+print(100 - 115)
+print(50 * 30)
+print(21 % 4)
+print(int(110 / 11))
+print(97 // 20)
