1dbd405dcff60a301f8517ee7f994c452b84f88e
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-06-24 18:14:52 +0300

    Assignment 1 Is Done Now ..

---
+++ Assignment_1.py
@@ -0,0 +1,38 @@
+# -----------------------
+# Assignment 1 
+# Moaz Osama 
+# -----------------------
+
+# Assignment 1 Done
+
+# Assignment 2 
+
+name = "Moaz osama"
+age = "19"
+country = "Egypt"
+
+# Assignment 3
+
+print("Name : " + name)
+print("Age : " + age)
+print("Country : " + country)
+
+print("Name : " + name + "\n"
+"Age : " + age + "\n"
+"Country : " + country)
+print( "=" * 30)
+
+# Assignment 4
+
+print("Hello, My Name Is " + name + " And Iam " + age + " Years Old and I Live in " + country + ".")
+print( "=" * 30)
+
+# Assignment 5
+
+print(type(name))
+print(type(age))
+print(type(country))
+print( "=" * 30)
+
+
+
