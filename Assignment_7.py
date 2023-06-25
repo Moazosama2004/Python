ee8e1d76c35535dbeee78bce078d8bab34780f78
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-06-25 20:33:37 +0300

    Assignment 7 Done

---
+++ Assignment_7.py
@@ -0,0 +1,48 @@
+# Assignment 1
+
+print(bool(1))
+print(bool(True))
+print(bool([10, 10, 10]))
+print(bool({"key": "value"}))
+print(bool(0))
+print(bool(None))
+print(bool(False))
+print(bool([]))
+print("=" * 30)
+
+# Assignment 2
+
+html = 80
+css = 60
+javascript = 70
+
+print(html > 50 and css > 50 and javascript > 50)
+print("=" * 30)
+
+# Assignment 3
+
+num_one = 10
+num_two = 20
+num = 20
+
+print(num > num_one or num > num_two)
+print(num > num_one and num > num_two)
+print("=" * 30)
+
+# Assignment 4
+
+num_one = 10
+num_two = 20
+
+result = num_one + num_two
+print(result)
+result **= 3
+print(result)
+result %= 26000
+print(result) 
+result /= 5
+print(result)
+print(type(str(result)))
+
+
+
