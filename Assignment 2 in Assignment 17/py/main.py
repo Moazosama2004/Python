7b3453963df3bef1c579c6ee4253c18db7a1662b
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-07-04 02:31:41 +0300

    Assignment 17 Done "Care With External File "

---
+++ Assignment 2 in Assignment 17/py/main.py
@@ -0,0 +1,15 @@
+# Assignment 2 
+
+# import my_mod 
+# print(my_mod.say_hello("Moaz"))
+# print(my_mod.say_welcome("Moaz"))
+
+# Assignment 3
+
+# from my_mod import say_welcome
+# print(say_welcome("Moaz"))
+
+# Assignment 4
+
+# from my_mod import say_welcome as  new_welcome 
+# print(new_welcome("Moaz"))
---
+++ Assignment_17.py
@@ -0,0 +1,13 @@
+# Assignment 1
+
+import random
+print(f"Random Number Between 10 And 50 => {random.randint(10,50)}")
+value = random.randint(2,10)
+print(f"Random Number Between 2 And 10 => { value if value % 2 == 0 else value + 1}")
+value = random.randint(1,9)
+print(f"Random Number Between 1 And 9 => { value if value % 2 != 0 else value + 1}")
+print(dir(random))
+print("=" * 30)
+
+# Assignment 2 and 3 and 4
+# In External File With Name Assignment 2 => Assignment 17
