b0926bd062a28edc508836e7b5f9f809b57468b2
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-07-08 07:08:01 +0300

    Assignment 22 Done

---
+++ Assingment_22.py
@@ -0,0 +1,46 @@
+# Assignment 1
+
+# Regular Expression : (\w)\s
+# "eeeeE llllLl lllzzZzzzz eroe operationr pollo "
+
+# Assignment 2
+
+# Regular Expression : [L](\w{,6})\d{2,}
+"EElzero11 LElzero111 ZElzero1111 EElzero11111 RElzero111111 OElzero1111111"
+
+# Assignment 3
+
+# Regular Expression : (\+?\(\d{4}\)\s\d{2,}-?\d{4})
+# +(0100) 600-1234
+# +(0100) 60-1234
+# (0100) 6000-1234
+# 01006001234
+# 0100 600 1234
+# (0100) 600-1
+# (0100) 600-12
+
+
+# Assignment 4
+
+# Regular Expression : (https?://(www\.)?\w+\.(org|com|net):?(\d+)?/(\w+\.\w+))
+# http://www.elzero.org:8888/link.php
+# https://elzero.org:8888/link.php
+# http://www.elzero.com/link.py
+# https://elzero.com/link.py
+# http://www.elzero.net
+# https://elzero.net
+
+# Assignment 5
+
+# Regular Expression : [h]\w+
+# Regular Expression : https?
+# Regular Expression : [shtp]
+# Regular Expression : [^abcd]
+# Regular Expression : h.+
+# http
+# https
+# abcd
+# abcd
+
+
+
