ae7a00b8c0c42aab0174167421f507f6ed01ff56
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-07-08 07:10:13 +0300

    Train file Edit

--- train.py
+++ train.py
@@ -1,1 +1,18 @@
-import re
+"""
+_summary_
+"""
+
+myFriends = ["Ahmed", "Osama", "Sayed"]
+
+def say_hello (some_peoples) -> list:
+    """_summary_
+    Args:
+        some_peoples (_type_): _description_
+
+    Returns:
+        list: _description_
+    """
+    for someone in some_peoples:
+        print(f"Hello {someone}")
+
+say_hello(myFriends)
